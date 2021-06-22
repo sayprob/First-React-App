f340fea19058c0060427207a2f86cf913aa0db5c
AITBENADDI Youssef <youssefaitbenaddi8@gmail.com>
2021-06-22 21:56:07 +0100

    Create Protected.js

---
+++ src/components/Protected.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+// import Header from "./Header";
+
+function Protected(props) {
+  let Cmp = props.Cmp;
+  const history = useHistory();
+  useEffect(() => {
+    if (!localStorage.getItem("user-info")) {
+      history.push("/register");
+    }
+  });
+  return (
+    <div>
+      <Cmp />
+    </div>
+  );
+}
+export default Protected;
